1a8020e00e2bdae0c8ad5697453d8b26fe6a0727
Maiax45 <116525897+Maiax45@users.noreply.github.com>
2026-04-04 13:18:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, request
-import os
-import requests
 
 app = Flask(__name__)
-TOKEN = "8625636756:AAHjtVORS0uNTX8q1VaFj3fRSjdzyrDW6TM"
 
 @app.route("/")
 def home():
@@ -11,22 +8,5 @@ def home():
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    print("Recebi uma mensagem do Telegram")
-    data = request.get_json()
-
-    if "message" in data:
-        chat_id = data["message"]["chat"]["id"]
-        text = data["message"].get("text", "")
-
-        if text == "/start":
-            enviar_mensagem(chat_id, "Bot funcionando 100%!")
-
+    print("Webhook recebido")
     return "ok", 200
-
-def enviar_mensagem(chat_id, texto):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {
-        "chat_id": chat_id,
-        "text": texto
-    }
-    requests.post(url, json=payload)
