719b59c7648c0523cd22ff56587e98071f62c4fd
Maiax45 <116525897+Maiax45@users.noreply.github.com>
2026-04-05 15:54:53 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
-from flask import Flask
-import threading
-import os
-from app import main
+from flask import Flask, render_template, request, redirect
+from database import listar_prestadores, excluir_prestador, criar_tabela
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="modelos")
 
-@app.route('/')
-def home():
-    return "Help Serviços Maiax está online!"
+criar_tabela()
 
-def iniciar_bot():
-    main()
+@app.route("/")
+def admin():
+    prestadores = listar_prestadores()
+    return render_template("admin.html", prestadores=prestadores)
+
+@app.route("/excluir", methods=["POST"])
+def excluir():
+    nome = request.form["nome"]
+    excluir_prestador(nome)
+    return redirect("/")
 
 if __name__ == "__main__":
-    iniciar_bot()
-else:
-    t = threading.Thread(target=iniciar_bot)
-    t.daemon = True
-    t.start()
+    app.run(host="0.0.0.0", port=5000)
