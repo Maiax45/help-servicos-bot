8df8593d8561d5db90d0e34713320b1ccc1fb7fc
Maiax45 <116525897+Maiax45@users.noreply.github.com>
2026-04-05 11:53:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -164,4 +164,8 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT, mensagens))
 
 print("Bot rodando...")
-app.run_polling()
+app.run_webhook(
+    listen="0.0.0.0",
+    port=8000,
+    webhook_url="https://SEU-PROJETO.up.railway.app"
+)
