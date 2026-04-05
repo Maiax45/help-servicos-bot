a6fafb251a730005cb9475cfe553d67864e02149
Maiax45 <116525897+Maiax45@users.noreply.github.com>
2026-04-05 09:46:33 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,20 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect
+from database import listar_prestadores, excluir_prestador, criar_tabela
+
+app = Flask(__name__, template_folder="modelos")
+
+criar_tabela()
 
 @app.route("/")
-def home():
-    return "SERVER OK"
+def admin():
+    prestadores = listar_prestadores()
+    return render_template("admin.html", prestadores=prestadores)
+
+@app.route("/excluir", methods=["POST"])
+def excluir():
+    nome = request.form["nome"]
+    excluir_prestador(nome)
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
