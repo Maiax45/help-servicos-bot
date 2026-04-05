3d1602bb97297f0948cb957c1737c6b20cca5c27
Maiax45 <116525897+Maiax45@users.noreply.github.com>
2026-04-05 13:00:17 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 import sqlite3
-from datetime import datetime, timedelta
 
 def conectar():
     return sqlite3.connect("banco.db")
@@ -15,10 +14,7 @@ def criar_tabela():
         servico TEXT,
         telefone TEXT,
         cidade TEXT,
-        data_cadastro TEXT,
-        data_vencimento TEXT,
-        status TEXT,
-        destaque INTEGER
+        status TEXT
     )
     """)
 
@@ -29,86 +25,27 @@ def adicionar_prestador(nome, servico, telefone, cidade):
     conn = conectar()
     c = conn.cursor()
 
-    data_cadastro = datetime.now()
-    vencimento = data_cadastro + timedelta(days=15)
-
-    c.execute("""
-    INSERT INTO prestadores 
-    (nome, servico, telefone, cidade, data_cadastro, data_vencimento, status, destaque)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-    """, (nome, servico, telefone, cidade, data_cadastro, vencimento, "Ativo", 0))
+    c.execute("INSERT INTO prestadores (nome, servico, telefone, cidade, status) VALUES (?, ?, ?, ?, ?)",
+              (nome, servico, telefone, cidade, "Ativo"))
 
     conn.commit()
     conn.close()
 
-def listar_por_servico(servico):
+def listar_prestadores():
     conn = conectar()
     c = conn.cursor()
 
-    hoje = datetime.now()
-
-    c.execute("""
-    SELECT nome, servico, telefone, cidade, destaque
-    FROM prestadores
-    WHERE servico LIKE ? AND status='Ativo' AND data_vencimento >= ?
-    ORDER BY destaque DESC
-    """, ('%' + servico + '%', hoje))
-
-    dados = c.fetchall()
-    conn.close()
-    return dados
-
-def listar_todos():
-    conn = conectar()
-    c = conn.cursor()
-
-    c.execute("SELECT nome, servico, status, data_vencimento FROM prestadores")
+    c.execute("SELECT nome, servico, telefone, cidade FROM prestadores WHERE status='Ativo'")
     dados = c.fetchall()
 
     conn.close()
     return dados
-
-def prestadores_vencendo():
-    conn = conectar()
-    c = conn.cursor()
-
-    c.execute("""
-    SELECT nome, servico, data_vencimento
-    FROM prestadores
-    WHERE date(data_vencimento) <= date('now', '+3 day')
-    """)
-
-    dados = c.fetchall()
-    conn.close()
-    return dados
-
-def ativar_pagamento(nome):
-    conn = conectar()
-    c = conn.cursor()
-
-    nova_data = datetime.now() + timedelta(days=30)
-
-    c.execute("""
-    UPDATE prestadores
-    SET data_vencimento=?, status='Ativo'
-    WHERE nome=?
-    """, (nova_data, nome))
-
-    conn.commit()
-    conn.close()
 
 def excluir_prestador(nome):
     conn = conectar()
     c = conn.cursor()
 
     c.execute("DELETE FROM prestadores WHERE nome=?", (nome,))
-    conn.commit()
-    conn.close()
 
-def tornar_destaque(nome):
-    conn = conectar()
-    c = conn.cursor()
-
-    c.execute("UPDATE prestadores SET destaque=1 WHERE nome=?", (nome,))
     conn.commit()
     conn.close()
